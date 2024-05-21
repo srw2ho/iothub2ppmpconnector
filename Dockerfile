f4540f831f98e6ec8e805e5a0db18b57d16f13af
balciselman <selman.balci@gmail.com>
2024-05-21 17:31:50 +0200

    update docker building

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,35 @@
-FROM python:slim-buster
+ARG PYTHON_VERSION="3.11-bookworm"
+
+FROM python:$PYTHON_VERSION
+
+# FROM python:3.9-slim-buster
+# FROM python:3.8-slim-buster
+# FROM python:3.8.0-slim
+
+
 
 RUN apt update && apt install -y git gcc
 RUN git config --global credential.helper cache
 
 COPY ./ ./
 
+# Enable Virtual Environment
+ENV VIRTUAL_ENV=/opt/venv
+RUN python -m venv $VIRTUAL_ENV
+RUN . $VIRTUAL_ENV/bin/activate
+
+RUN pip install --upgrade pip
+
 RUN pip install Cython
-
-# RUN pip install -r requirements.txt
-
-# RUN pip install setup.py
-
-
 
 
 RUN pip install git+https://github.com/srw2ho/iothub2ppmpconnector.git
 
-# ENTRYPOINT python -u -m ppmp2influx
+
+RUN pip install  pip-licenses
+RUN pip-licenses --with-system --with-urls --order=license > ./python_dependencies.txt
+
+RUN pip uninstall -y Cython pip-licenses 
+
 
 ENTRYPOINT tail -f /dev/null
---
+++ builddocker.sh
@@ -0,0 +1,42 @@
+
+git describe --tags --abbrev=0  > dockerversion.txt
+echo -e "lese aus Datei dockerversion.txt die Version und schreibe diese into Build"
+
+
+# Build=<dockerversion.txt
+
+Build=$(<dockerversion.txt)
+
+
+# PYTHON_INSTALLVERSION=3.11-bookworm # for RASPI 5
+PYTHON_INSTALLVERSION=3.11-slim-buster # for RASPI 5
+# PYTHON_INSTALLVERSION=3.9-slim-bookworm # for RASPI 4
+# PYTHON_INSTALLVERSION=3.9-bookworm # for RASPI 4
+# PYTHON_INSTALLVERSION=3.9-slim-buster # for RASPI 4 funktioniert nicht mit lgpio
+
+dockerimage="iothub2ppmpconnector"
+
+if [ "${Build}" == "" ]; then
+    Build="latest"
+fi
+
+
+# echo -e "$Build"
+
+
+echo -e " build dockerimage: $dockerimage  Build Version:" $Build
+
+
+
+
+
+
+# sudo docker build --build-arg NODE_ENV=development --build-arg BUILDCMD=dev -f Dockerfile.MH -t grafana/grafana-mhdev:v7.6.5 .
+
+sudo docker build -t ${dockerimage}:${Build}  --build-arg PYTHON_VERSION=${PYTHON_INSTALLVERSION} -f Dockerfile .
+
+# @docker build . -t %dockerimage%:%Build%
+
+# save images in ../docker_images
+mkdir ../docker_images
+docker save -o ../docker_images/${dockerimage}_${Build}.zip ${dockerimage}:${Build}
